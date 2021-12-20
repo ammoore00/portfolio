0e20733124b00cd06530f407abe2d06607b793e0
Abigail Moore <abigailmmoore00@gmail.com>
2021-12-20 13:45:15 -0700

    Tweaked more descriptions

--- config/config.js
+++ config/config.js
@@ -45,7 +45,7 @@ export const intro = {
 export const about = {
 	title: "Who I am",
 	description: [
-		"I attended Colorado School of Mines from 2016 to 2021 studying Computer Science with a focus on Data Science, and worked as a data scientist at AudibleHealthAI from 2020 to 2021.",
+		"I attended Colorado School of Mines from 2016 to 2021 studying Computer Science with a focus on Data Science, and worked as a data science intern at AudibleHealthAI from 2020 to 2021.",
 		"I enjoy modding video games, primarily Minecraft, and have also contributed to several open source tools to help others begin modding as well. I have also created several machine learning models leveraging computer vision for things such as motion tracking and object recognition. All of my projects are available on my github profile, while a curated selection are linked in the projects section below.",
 		"All of my code is provided under an open license to help future developers learn and to encourage growth in the tech community.",
 		"When I’m not programming, I focus on my other hobbies which are: rock climbing, skiing, scuba diving, reading scifi and fantasy, and playing D&D.",
@@ -62,7 +62,7 @@ export const work = {
 		},
 		{
 			title: "Game Design",
-			description: "I create mods and standalone games, primarily using Java and C++.",
+			description: "I create mods and standalone games, primarily using Java and C++, under the pseudonym Dawnraider.",
 			icons: null,
 		}
 	],
